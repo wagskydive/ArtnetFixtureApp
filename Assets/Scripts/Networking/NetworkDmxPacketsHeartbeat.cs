048842d5ef6cafd26adab85f045df0144b822612
wagskydive <wagskydive@gmail.com>
2026-04-19 13:38:46 +0200

    fixed network warning not showing on start

--- Assets/Scripts/Networking/NetworkDmxPacketsHeartbeat.cs
+++ Assets/Scripts/Networking/NetworkDmxPacketsHeartbeat.cs
@@ -5,6 +5,17 @@ public static class NetworkDmxPacketsHeartbeat
     private static long _lastPacketTicks;
     public static long LastPacketTicks => _lastPacketTicks;
 
+    static NetworkDmxPacketsHeartbeat()
+    {
+
+        Initialize();
+    }
+
+    public static void Initialize()
+    {
+        _lastPacketTicks = DateTime.UtcNow.Ticks;
+    }
+
     public static void NotifyPacketReceived()
     {
         _lastPacketTicks = DateTime.UtcNow.Ticks;
